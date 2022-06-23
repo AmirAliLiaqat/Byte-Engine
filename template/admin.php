d85fcc11592d7be02b2815dcc1231c1d1dec127c
Amir Liaqat <amirliaqat2020@gmail.com>
2022-06-23 17:12:50 +0500

    Update admin page slug...

--- template/admin.php
+++ template/admin.php
@@ -4,35 +4,9 @@
     <h2>Manage All Settings</h2>
     <p>Manage your all settings just clicking on checkbox...</p>
     <form action="options.php" method="post">
-        <div class="my_wrap" style="">
-            <label for="title">Title</label>
-            <input type="checkbox" name="title" value="title" class="toggle"></br>
-            <label for="editor">Editor</label>
-            <input type="checkbox" name="editor" value="editor"></br>
-            <label for="feature-img">Featured Image</label>
-            <input type="checkbox" name="feature-img" value="feature-img"></br>
-            <label for="excerpt">Excerpt</label>
-            <input type="checkbox" name="excerpt" value="excerpt"></br>
-            <label for="trackbacks">Trackbacks</label>
-            <input type="checkbox" name="trackbacks" value="trackbacks"></br>
-            <label for="custom-fields">Custom Fields</label>
-            <input type="checkbox" name="custom-fields" value="custom-fields"></br>
-            <label for="comments">Comments</label>
-            <input type="checkbox" name="comments" value="comments"></br>
-            <label for="revisions">Revisions</label>
-            <input type="checkbox" name="revisions" value="revisions"></br>
-            <label for="author">Author</label>
-            <input type="checkbox" name="author" value="author"></br>
-            <label for="page-attributes">Page Attributes</label>
-            <input type="checkbox" name="page-attributes" value="page-attributes"></br>
-            <label for="post-formats">Post Formats</label>
-            <input type="checkbox" name="post-formats" value="post-formats"></br>
-            <label for="none">None</label>
-            <input type="checkbox" name="none" value="none"></br>
-        </div><!--my_wrap-->
         <?php
-            settings_fields( '' );
-            do_settings_sections( '' );
+            settings_fields( 'bb_main_setting' );
+            do_settings_sections( 'bb_main_setting' );
             submit_button();
         ?>
     </form>
