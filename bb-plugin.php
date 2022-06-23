3c000423f04e01dbf90bf030e737db2526b60ab5
Amir Liaqat <amirliaqat2020@gmail.com>
2022-06-23 12:55:10 +0500

    Creating plugin main file and adding plugin identity

---
+++ bb-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: BB Plugin
+ * Plugin URI: https://amir.bytebunch.com/plugins
+ * Author: Byte Ki Duniya
+ * Author URI: https://amir.bytebunch.com
+ * Description: That plugin which is used for multi purposes like(Custom Forms, Custom Post Types, Live Chat, Word and Character Count etc).
+ * Version: 1.0.0
+ * License: GPL v2 or Later
+ * Text Domain: bb_plugin
+ */
